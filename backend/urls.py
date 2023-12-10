0bad03b2352857302b5a076964fbc5e600aeee5e
Piyush <piyush.kalyani7@gmail.com>
2023-12-10 17:43:25 +0530

    remove direct access

--- backend/urls.py
+++ backend/urls.py
@@ -19,10 +19,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.http import HttpResponse
+
+def server_working(request):
+    return HttpResponse("Server is working!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html')),
+    # path('', TemplateView.as_view(template_name='index.html')),
+    path('', server_working),
     path('api/products/', include('base.urls.product_urls')),
     path('api/users/', include('base.urls.user_urls')),
     path('api/orders/', include('base.urls.order_urls')),
